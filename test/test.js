a6053169e5b340cd45712206aaeeb9d6a36acd42
MariaHu <marieastrid.hugel@gmail.com>
2023-07-05 15:10:21 +0200

    beginning with responsive design

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+const { it } = require('mocha');
+
+
+describe('index', function () {
+  describe('click()', function () {
+
+    it.todo("toggle the local variable between dark and light"), function() {
+    
+    };
+    it.todo('toggle the css between styles_dark.css and styles_light.css '),
+     function () {
+        /*Context: styles_light.css ist active
+        or await page charging ? 
+        /* retrieve value of the attribute 
+        test if value when click() is the same as "dark_styles.css"
+        otherwise, throw an error.         
+        */
+
+    }
+});
+  });
